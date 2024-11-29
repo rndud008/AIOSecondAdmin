1888ec8ae061d5d64a149041647c5ec04e2b3d1e
rndud008 <rndud008@gamil.com>
2024-11-29 20:42:15 +0900

    init

---
+++ src/test/java/hello/aiosecondadmin/AioSecondAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.aiosecondadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AioSecondAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
